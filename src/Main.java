ba52b005702381edb6a10b519fe0079165b6b659
bthouverez <bastien.thouverez@gmail.com>
2024-10-15 11:23:45 +0200

    Début de DAO

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import database.DAO;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        DAO dao = new DAO();
+        dao.run();
     }
 }

96ba622dfcfea1f72bf56aa5bcc9338b42b899a2
Sopsz <ethandeglos@gmail.com>
2024-11-05 10:44:42 +0100

    le grand remplacement

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        // Weapon weapon1_str = new Weapon(1 ,"test1" ,1 ,20 ,0 ,0 ,skill1_weap1_str ,skill2_weap1_str);
-        // Weapon weapon2_str = new Weapon(2 ,"test2" ,2 ,15 ,5 ,0 ,skill1_weap2_str ,skill2_weap2_str);
-        // Weapon weapon3_str = new Weapon(3 ,"test3" ,3 ,15 ,0 ,5 ,skill1_weap3_str ,skill2_weap3_str);
-
-        // Weapon weapon1_agi = new Weapon(4 ,"test4" ,4 ,0 ,20 ,0 ,skill1_weap1_agi ,skill2_weap1_agi);
-        // Weapon weapon2_agi = new Weapon(5 ,"test5" ,5 ,0 ,15 ,5 ,skill1_weap2_agi ,skill2_weap2_agi);
-        // Weapon weapon3_agi = new Weapon(6 ,"test6" ,6 ,5 ,15 ,5 ,skill1_weap3_agi ,skill2_weap3_agi);
-
-        // Weapon weapon1_int = new Weapon(7 ,"test7" ,7 ,0 ,0 ,20 ,skill1_weap1_int ,skill2_weap1_int);
-        // Weapon weapon2_int = new Weapon(8 ,"test8" ,8 ,0 ,5 ,15 ,skill1_weap2_int ,skill2_weap2_int);
-        // Weapon weapon3_int = new Weapon(9 ,"test9" ,9 ,5 ,0 ,15 ,skill1_weap3_int ,skill2_weap3_int);
+        //
 
     }
 }
--- src/Weapon.java
+++ src/Weapon.java
@@ -2,30 +2,21 @@ public class Weapon {
     private int id;
     private String name;
     private int speed;
-    private int strength;
-    private int agility;
-    private int intelligence;
     private Skill skill1;
     private Skill skill2;
 
-    public Weapon()
-    {
+    public Weapon() {
         this.id = 0;
         this.name = "test_weapon";
         this.speed = 1;
-        this.strength = 5;
-        this.agility = 5;
-        this.intelligence = 5;
-        this.skill1 = new Attack();
-        this.skill2 = new Attack();
+
+        this.skill1 = new Skill();
+        this.skill2 = new Skill();
     }
-    public Weapon(int id, String name, int speed, int strength, int agility, int intelligence, Skill skill1, Skill skill2) {
+    public Weapon(int id, String name, int speed, Skill skill1, Skill skill2) {
         this.id = id;
         this.name = name;
         this.speed = speed;
-        this.strength = strength;
-        this.agility = agility;
-        this.intelligence = intelligence;
         this.skill1 = skill1;
         this.skill2 = skill2;
     }
@@ -39,22 +30,12 @@ public class Weapon {
     public int getSpeed() {
         return speed;
     }
-    public int getStrength() {
-        return strength;
-    }
-    public int getAgility() {
-        return agility;
-    }
-    public int getIntelligence() {
-        return intelligence;
-    }
     public Skill getSkill1() {
         return skill1;
     }
     public Skill getSkill2() {
         return skill2;
     }
-
 
     public void setId(int id) {
         this.id = id;
@@ -64,15 +45,6 @@ public class Weapon {
     }
     public void setSpeed(int speed) {
         this.speed = speed;
-    }
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-    public void setAgility(int agility) {
-        this.agility = agility;
-    }
-    public void setIntelligence(int intelligence) {
-        this.intelligence = intelligence;
     }
     public void setSkill1(Skill skill1) {
         this.skill1 = skill1;
